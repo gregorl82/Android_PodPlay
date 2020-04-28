1b6d968046c6994b627cc25ee23b4ee51fc3e81c
Gregor <gregorlennie@hotmail.co.uk>
2020-04-28 12:29:12 +0100

    Adds ItunesRepo and verifies successful response

---
+++ app/src/main/java/com/example/android/podplay/repository/ItunesRepo.kt
@@ -0,0 +1,31 @@
+package com.example.android.podplay.repository
+
+import com.example.android.podplay.service.ItunesService
+import com.example.android.podplay.service.PodcastResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ItunesRepo(private val itunesService: ItunesService) {
+
+    fun searchByTerm(term: String, callBack: (List<PodcastResponse.ItunesPodcast>?) -> Unit) {
+
+        val podcastCall = itunesService.searchPodcastByTerm(term)
+
+        podcastCall.enqueue(object: Callback<PodcastResponse> {
+
+            override fun onFailure(call: Call<PodcastResponse>?, t: Throwable?) {
+                callBack(null)
+            }
+
+            override fun onResponse(
+                call: Call<PodcastResponse>?,
+                response: Response<PodcastResponse>?
+            ) {
+                val body = response?.body()
+
+                callBack(body?.results)
+            }
+        })
+    }
+}
--- app/src/main/java/com/example/android/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/example/android/podplay/ui/PodcastActivity.kt
@@ -2,12 +2,23 @@ package com.example.android.podplay.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.android.podplay.R
+import com.example.android.podplay.repository.ItunesRepo
+import com.example.android.podplay.service.ItunesService
 
 class PodcastActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
+
+        val TAG = javaClass.simpleName
+
+        val itunesService = ItunesService.instance
+
+        val itunesRepo = ItunesRepo(itunesService)
+
+        itunesRepo.searchByTerm("Android Developer", {Log.i(TAG, "Results = $it")})
     }
 }
