744ce3e9271916b3a130fb992d24c2b843fa074b
Gregor <gregorlennie@hotmail.co.uk>
2020-04-28 14:12:31 +0100

    Adds search bar to main activity

--- app/src/main/java/com/example/android/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/example/android/podplay/ui/PodcastActivity.kt
@@ -1,8 +1,12 @@
 package com.example.android.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.example.android.podplay.R
 import com.example.android.podplay.repository.ItunesRepo
 import com.example.android.podplay.service.ItunesService
@@ -20,5 +24,19 @@ class PodcastActivity : AppCompatActivity() {
         val itunesRepo = ItunesRepo(itunesService)
 
         itunesRepo.searchByTerm("Android Developer", {Log.i(TAG, "Results = $it")})
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
     }
 }
